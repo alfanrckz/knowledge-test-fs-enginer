3ffce6e598ee6ac2767c777b34427a460661b352
Alfanrckz <alfansyuriziaulhaq11@gmail.com>
2024-05-21 14:48:24 +0700

    feat: product-services

--- backend/src/services/ProductServices.ts
+++ backend/src/services/ProductServices.ts
@@ -1,13 +1,33 @@
 import { Repository } from "typeorm";
 import cloudinary from "../libs/cloudinary";
-import { createProductSchema } from "../utils/validator/product";
+import { createProductSchema, updateProductSchema } from "../utils/validator/product";
 import { validate } from "../utils/validator/validation";
 import { Product } from "../entity/Product";
 import { AppDataSource } from "../data-source";
+import { Request, Response } from "express";
 
 export default new (class ProductServices {
   private readonly productRepository: Repository<Product> =
     AppDataSource.getRepository(Product);
+
+  async getProducts(){
+    return this.productRepository.find({
+
+      order: {
+        id: "DESC"
+      },
+      select: 
+      {
+        id: true,
+          name: true,
+          description: true,
+          image: true,
+          price: true,
+      }
+    })
+  }
+
+
   async create(data: any) {
     const isValid = validate(createProductSchema, data);
     cloudinary.config();
@@ -26,4 +46,47 @@ export default new (class ProductServices {
       data: response,
     };
   }
+
+  async updateProduct(data: any, id: any) {
+    const isValid = validate(updateProductSchema, data);
+    cloudinary.config();
+
+    let valid: any = {
+        name: isValid.name,
+        price: isValid.price,
+        description: isValid.description,
+        updated_at: isValid.updated_at
+    };
+
+    if (isValid.image) {
+        const upfile = await cloudinary.upload(isValid.image);
+        valid.image = upfile.secure_url;
+    }
+
+    // Filter out undefined properties
+    valid = Object.fromEntries(Object.entries(valid).filter(([_, v]) => v != null));
+
+    if (!valid.name) {
+        throw new Error("Product name is required");
+    }
+
+    await this.productRepository.update({ id: id.id }, valid);
+
+    return {
+        message: "Product updated successfully",
+        data: valid,
+    };
+}
+
+async deleteProduct(id){
+  const checkProduct = await this.productRepository.countBy(id);
+  if(checkProduct === 0) throw new Error("Product not found");
+
+  const response = await this.productRepository.delete(id);
+  return {
+    message: "Product deleted successfully",
+  }
+}
+
+
 })();
